f7c3f5dd1e4040cf013c89341243348737617d7c
unknown <fabriciogarciacarvalho87@hotmail.com>
2024-11-10 16:13:08 -0300

    correcao terceiro nivel

--- 03-Terceiro-nível/01-sample.js
+++ 03-Terceiro-nível/01-sample.js
@@ -2,18 +2,11 @@ async function buscarEDetalharNave() {
     try {
         const url = "https://swapi.dev/api/starships/9/";
         const resposta = await fetch(url);
-
-       
         if (!resposta.ok) {
             throw new Error(`Erro ao buscar nave: ${resposta.statusText}`);
         }
-
         const nave = await resposta.json();
-
-        
         const { name, model, manufacturer, crew } = nave;
-
-        
         const detalhesNave = `
 Nome: ${name}
 Modelo: ${model}
@@ -21,13 +14,10 @@ Fabricante: ${manufacturer}
         `;
         console.log("Detalhes da Nave:");
         console.log(detalhesNave);
-
-        
         const tamanhoTripulacao = parseInt(crew, 10);
-
         if (isNaN(tamanhoTripulacao)) {
             console.log("Informação de tripulação inválida.");
-        } else if (tamanhoTripulacao > 100) {
+        } else if (tamanhoTripulacao > "100") {
             console.log("Esta é uma nave grande.");
         } else {
             console.log("Esta é uma nave pequena.");
